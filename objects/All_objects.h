3205d1dbac1f100983a82e4f6519a496dc670561
Chris Cunningham <chris.c.cunning@gmail.com>
2018-07-07 19:00:56 +1000

    Missed header in commit

---
+++ objects/All_objects.h
@@ -0,0 +1,12 @@
+#ifndef ALL_OBJECTS_H
+#define ALL_OBJECTS_H
+
+#include "objects/TObject.h"
+#include "objects/TGame.h"
+#include "objects/board/TBoard.h"
+#include "objects/board/weapon/TWeapon.h"
+#include "objects/board/weapon/TTurret.h"
+#include "objects/control/TControl.h"
+#include "objects/storage/TStorage.h"
+
+#endif // ALL_OBJECTS_H
